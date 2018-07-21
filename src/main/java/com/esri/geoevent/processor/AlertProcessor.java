50366049f674377598abec71c43ebfbcb664fa6b
kelseymurdock <kmurdock8@gmail.com>
2018-07-20 22:31:29 -0700

    psuedocode to prepare for hackathon

--- src/main/java/com/esri/geoevent/processor/AlertProcessor.java
+++ src/main/java/com/esri/geoevent/processor/AlertProcessor.java
@@ -16,6 +16,11 @@ public class AlertProcessor extends AsyncGeoEventProcessor
    * See {@link BundleLogger} for more info.
    */
   	private static final BundleLogger LOGGER = BundleLoggerFactory.getLogger(AlertProcessor.class);
+
+  	// User input
+    private String lostDef;
+    private String sightingDef;
+    private String foundDef;
   
 
 
@@ -28,6 +33,46 @@ public class AlertProcessor extends AsyncGeoEventProcessor
 	@Override
 	public GeoEvent process(GeoEvent geoEvent) throws Exception
 	{
+	    // get geoevent def name
+
+        // Switch on def name
+        // case lostDef:
+
+            // query db and get all of the users that have their notifications set to on
+            // get their email, lat, and long of the address they provided = usersout
+
+            // get search geometry from the geoevent definition
+            // for each user in usersout:
+                // create point from lat and long values
+                // use geometry engine to see if these two geometries intersect
+
+                // if they do intersect:
+                    // create new email alert def with the users email
+                    // add email alert geoevent to the async processor blocking queue to send it out asysncrously
+
+
+            //return null;
+        // case sightingdef:
+
+            // get owner email from geoevent and send them an alert first
+
+            // get all users that have notifications set to on (make this a global variable maybe?)
+            // get their email, lat, and long of the address they provided = usersout
+
+            // get search geo from geoevent
+            // for each user in userout:
+                // check if user has already been notified from watching list
+                // if not:
+                    // see if the points intersect (might be faster to check this first)
+                    // if they do intersect:
+                        // create new email alert geoevent
+                        // send it out
+
+
+        // case foundDef:
+
+	        // send owner new email alert
+
 		return geoEvent;
 	}
 
